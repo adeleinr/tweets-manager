09fd66dfc6d7b9b1f6caef0ad553a44fbdccb6db
Adelein Rodriguez <adeleinr@gmail.com>
2023-04-20 15:26:22 -0700

    Linting

--- hashtags.py
+++ hashtags.py
@@ -3,16 +3,22 @@ import json
 import unittest
 from collections import defaultdict, Counter
 
+
 class HashtagManager:
     def __init__(self):
         self.average_degree = 0
         self.total_edges = 0
         self.total_degree_sum = 0
+        # {"tagA": ["tagB", "tagC"]}
         self.graph = defaultdict(set)
+        # {"tagA": (num times tagA is referenced across all tweets)}
+        # This is used for figuring out if a tag can be removed from
+        # the graph completely when a tweet is deleted
         self.tag_counter = Counter()
 
     def build_graph(self, tweet_data_file):
-        # Using input for reading one line at a time instead of bringing the whole file into memory
+        # Using input for reading one line at a time instead of bringing
+        # the whole file into memory
         for line in fileinput.input([tweet_data_file]):
             tweet = json.loads(line)
             if "entities" in tweet:
@@ -87,7 +93,8 @@ class HashtagManager:
         self.build_graph(tweets_file)
 
     def delete_tweet(self, tweet_file):
-        # Using input for reading one line at a time instead of bringing the whole file into memory
+        # Using input for reading one line at a time instead of bringing
+        # the whole file into memory
         for line in fileinput.input([tweet_file]):
             tweet = json.loads(line)
             if "entities" in tweet:
@@ -98,99 +105,80 @@ class HashtagManager:
                 self.delete(hashtags)
 
 
-
-"""
-hashtag_manager = HashtagManager("tweets_test.txt")
-print(hashtag_manager.total_degree_sum)
-print(hashtag_manager.compute_average_degree())
-
-print(hashtag_manager.add_new_tweets("new_tweets_test.txt"))
-print(hashtag_manager.total_degree_sum)
-print(hashtag_manager.compute_average_degree())
-
-"""
-
-hashtag_manager = HashtagManager()
-hashtag_manager.build_graph("tweets.txt")
-print(hashtag_manager.total_degree_sum)
-print(hashtag_manager.compute_average_degree())
-hashtag_manager.reset_graph()
-
-
 class TestTweetParser(unittest.TestCase):
     hashtag_manager = HashtagManager()
 
     def test_loading_from_file(self):
-        hashtag_manager.build_graph("tweets.txt")
-        assert(abs(hashtag_manager.compute_average_degree() - 2.488) <= 0.01)
-        hashtag_manager.reset_graph()
+        self.hashtag_manager.build_graph("tweets.txt")
+        assert (abs(
+            self.hashtag_manager.compute_average_degree() - 2.488) <= 0.01
+        )
+        self.hashtag_manager.reset_graph()
 
     def test_sunnyday(self):
-        hashtag_manager.reset_graph()
-        hashtag_manager.add(["Gretel", "data"])
-        hashtag_manager.add(["data", "startup", "privacy"])
-        hashtag_manager.add(["data"])
-        hashtag_manager.add(["Gretel"])
-        hashtag_manager.add(["rocketship", "Gretel"])
-        hashtag_manager.add(["cats", "cats", "cats"])
-        assert(abs(hashtag_manager.compute_average_degree() - 1.67) <= 0.01)
+        self.hashtag_manager.reset_graph()
+        self.hashtag_manager.add(["Gretel", "data"])
+        self.hashtag_manager.add(["data", "startup", "privacy"])
+        self.hashtag_manager.add(["data"])
+        self.hashtag_manager.add(["Gretel"])
+        self.hashtag_manager.add(["rocketship", "Gretel"])
+        self.hashtag_manager.add(["cats", "cats", "cats"])
+        assert (abs(
+            self.hashtag_manager.compute_average_degree() - 1.67) <= 0.01
+        )
 
     def test_tweets_with_same_tags_arent_counted(self):
-        hashtag_manager.reset_graph()
-        hashtag_manager.add(["Gretel", "ai"])
-        hashtag_manager.add(["Gretel", "ai"])
-        hashtag_manager.add(["ai", "Gretel"])
-        assert(hashtag_manager.compute_average_degree() == 1)
+        self.hashtag_manager.reset_graph()
+        self.hashtag_manager.add(["Gretel", "ai"])
+        self.hashtag_manager.add(["Gretel", "ai"])
+        self.hashtag_manager.add(["ai", "Gretel"])
+        assert (self.hashtag_manager.compute_average_degree() == 1)
 
     def test_a_tag_duplicated_in_a_tweet_isnt_counted(self):
-        hashtag_manager.reset_graph()
-        hashtag_manager.add(["Gretel", "Gretel"])
-        assert(hashtag_manager.compute_average_degree() == 0)
+        self.hashtag_manager.reset_graph()
+        self.hashtag_manager.add(["Gretel", "Gretel"])
+        assert (self.hashtag_manager.compute_average_degree() == 0)
 
     def test_case_insensitive(self):
-        hashtag_manager.reset_graph()
-        hashtag_manager.add(["Gretel", "ai"])
-        hashtag_manager.add(["gretel", "ai"])
-        assert(hashtag_manager.compute_average_degree() == 1)
+        self.hashtag_manager.reset_graph()
+        self.hashtag_manager.add(["Gretel", "ai"])
+        self.hashtag_manager.add(["gretel", "ai"])
+        assert (self.hashtag_manager.compute_average_degree() == 1)
 
     def test_case_delete_tweet_tag_added_before(self):
-        hashtag_manager.reset_graph()
-        print(hashtag_manager.graph)
-        hashtag_manager.add(["Gretel"])
-        hashtag_manager.add(["dragons", "tacos"])
-        hashtag_manager.add(["gretel", "ai"])
+        self.hashtag_manager.reset_graph()
+        self.hashtag_manager.add(["Gretel"])
+        self.hashtag_manager.add(["dragons", "tacos"])
+        self.hashtag_manager.add(["gretel", "ai"])
         # 4 vertices
-        assert(hashtag_manager.compute_average_degree() == 1)
-        hashtag_manager.delete(["gretel", "ai"])
+        assert (self.hashtag_manager.compute_average_degree() == 1)
+        self.hashtag_manager.delete(["gretel", "ai"])
         # 3 vertices, 3 edges
-        assert(abs(hashtag_manager.compute_average_degree() - 0.66) <= 0.01)
+        assert (abs(
+            self.hashtag_manager.compute_average_degree() - 0.66) <= 0.01
+        )
 
     def test_case_delete_tweet_tag_not_added_before(self):
-        hashtag_manager.reset_graph()
-        hashtag_manager.add(["dragons", "tacos"])
-        hashtag_manager.add(["gretel", "ai"])
+        self.hashtag_manager.reset_graph()
+        self.hashtag_manager.add(["dragons", "tacos"])
+        self.hashtag_manager.add(["gretel", "ai"])
         # 4 vertices,  4 edges
-        assert(hashtag_manager.compute_average_degree() == 1)
-        hashtag_manager.delete(["gretel", "ai"])
+        assert (self.hashtag_manager.compute_average_degree() == 1)
+        self.hashtag_manager.delete(["gretel", "ai"])
         # 2 vertices, 2 edges
-        assert(hashtag_manager.compute_average_degree() == 1)
+        assert (self.hashtag_manager.compute_average_degree() == 1)
 
     def test_case_add_delete_add_in_sequence(self):
-        hashtag_manager.reset_graph()
-        hashtag_manager.add(["dragons", "tacos"])
-        hashtag_manager.add(["gretel", "ai"])
+        self.hashtag_manager.reset_graph()
+        self.hashtag_manager.add(["dragons", "tacos"])
+        self.hashtag_manager.add(["gretel", "ai"])
         # 4 vertices,  4 edges
-        assert(hashtag_manager.compute_average_degree() == 1)
-        hashtag_manager.delete(["gretel", "ai"])
+        assert (self.hashtag_manager.compute_average_degree() == 1)
+        self.hashtag_manager.delete(["gretel", "ai"])
         # 2 vertices, 2 edges
-        assert(hashtag_manager.compute_average_degree() == 1)
-        hashtag_manager.add(["gretel", "ai"])
-        assert(hashtag_manager.compute_average_degree() == 1)
+        assert (self.hashtag_manager.compute_average_degree() == 1)
+        self.hashtag_manager.add(["gretel", "ai"])
+        assert (self.hashtag_manager.compute_average_degree() == 1)
+
 
 unittest.main()
-
-
-
-
-
-
